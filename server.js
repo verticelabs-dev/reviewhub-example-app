e62be540915bedee2add3c3df9bcd5ecccb9258e
Travier Moorlag <moorlagt@gmail.com>
2023-01-16 22:58:16 -0500

    test events

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-    console.log('request was received')
+app.get("/", (req, res) => {
+  console.log("request was received");
 
-  res.send('Hello World from New PR!!');
+  res.send("Hello World from New PR2!!");
 });
 
 app.listen(PORT, HOST, () => {
